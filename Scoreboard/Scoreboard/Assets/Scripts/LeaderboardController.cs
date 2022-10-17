def0bad1bea5ff95d6703900d754a24bef55b889
Alexander-Koutrakis <61294700+Alexander-Koutrakis@users.noreply.github.com>
2022-10-17 15:05:33 +0300

    17/10

--- Scoreboard/Scoreboard/Assets/Scripts/LeaderboardController.cs
+++ Scoreboard/Scoreboard/Assets/Scripts/LeaderboardController.cs
@@ -1,16 +1,19 @@
-using System.Collections;
+
 using LootLocker.Requests;
 using UnityEngine;
-
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 public class LeaderboardController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private int ID;
+    private Action onSessionStarted;
     void Start()
     {
-        StartCoroutine(StartSession());
+        StartSession();
     }
 
-    private IEnumerator StartSession()
+    private async void StartSession()
     {
         bool done = false;
         LootLockerSDKManager.StartGuestSession((response) =>
@@ -25,11 +28,151 @@ public class LeaderboardController : MonoBehaviour
             {
                 Debug.Log("Couldnt start session");
                 done = true;
+                StartSession();
             }
 
         });
 
-        yield return new WaitWhile(() => done == false);
+        while (!done)
+        {
+            await Task.Yield();
+        }
+
+        onSessionStarted?.Invoke();
     }
 
+    public async Task SubmitScore(string username,int score)
+    {
+        bool done = false;
+        LootLockerSDKManager.SubmitScore(username, score, ID, (response) => {
+
+            if (response.success)
+            {
+                Debug.Log("Success");
+                done = true;
+            }
+            else
+            {
+                Debug.Log("Failure");
+                done = true;
+            }
+
+        });
+
+        while (!done)
+        {
+            await Task.Yield();
+        }
+    }
+    public async Task<LootLockerLeaderboardMember[]> GetScores(int startRank,int amount)
+    {
+        bool done = false;
+        LootLockerLeaderboardMember[] lootLockerLeaderboardMembers=new LootLockerLeaderboardMember[0];
+        LootLockerSDKManager.GetScoreList(ID,amount,startRank, (response) =>
+        {
+            if (response.success)
+            {                
+                Debug.Log("Successful");
+                lootLockerLeaderboardMembers = response.items;
+                done = true;
+            }
+            else
+            {
+                Debug.Log("failed: " + response.Error);
+                done = true;
+            }
+        });
+
+        while (!done)
+        {
+            await Task.Yield();
+        }
+
+        return lootLockerLeaderboardMembers;
+    }  
+     public async Task<int> Test()
+    {
+        bool done = false;
+        int totalRanks = 0;
+        LootLockerSDKManager.GetScoreList(ID,0, -1, (response) =>
+          {
+              if (response.success)
+              {
+                  Debug.Log("Successful");
+                  totalRanks = response.pagination.total;
+                  done = true;
+              }
+              else
+              {
+                  Debug.Log("failed: " + response.Error);
+                  done = true;
+              };
+          });
+
+        while (!done)
+        {
+            await Task.Yield();
+        }
+
+        return totalRanks;
+    }
+   
+    private async Task SubmitRandomScore()
+    {
+        string randomUser = RandomString(8);
+        int randomScore = RandomInt();
+        bool done = false;
+        LootLockerSDKManager.SubmitScore(randomUser, randomScore, ID, (response) => {
+
+            if (response.success)
+            {
+                Debug.Log("Success");
+                done = true;
+            }
+            else
+            {
+                Debug.Log("Failure "+response.Error);
+                done = true;
+
+            }
+
+        });
+
+        while (!done)
+        {
+            await Task.Yield();
+        }
+    }
+
+    public void AddOnSessionStartedAction(Action action)
+    {
+        onSessionStarted += action;
+    }
+
+    public void RemoveOnSessionStartedAction(Action action)
+    {
+        onSessionStarted -= action;
+    }
+
+    //Used for testing
+    public async void AddRandomScores(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+             await SubmitRandomScore();
+        }
+    }
+
+    private System.Random random = new System.Random();
+    private string RandomString(int length)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+    }
+
+    private int RandomInt()
+    {
+        return UnityEngine.Random.Range(1, 9999);
+    }
 }
---
+++ Scoreboard/Scoreboard/Assets/Scripts/LeaderboardPresentationController.cs
@@ -0,0 +1,73 @@
+    
+using UnityEngine;
+using LootLocker.Requests;
+using TMPro;
+using System.Threading.Tasks;
+
+public class LeaderboardPresentationController : MonoBehaviour
+{
+    private int highestShownRank=0;
+    private int lowestShownRank;
+    private int shownRanks = 10;
+    private LeaderboardController leaderboardController;
+    [SerializeField]private GameObject[] leaderboardMemberGOArray;
+
+    private void Awake()
+    {
+        leaderboardController = FindObjectOfType<LeaderboardController>();
+        SubmitScoreController submitScoreController = FindObjectOfType<SubmitScoreController>();
+        submitScoreController.AddOnSubmitAction(RefreshPage);
+        leaderboardController.AddOnSessionStartedAction(RefreshPage);
+    }
+    public void ShowTop()
+    {
+        highestShownRank = -100;
+        RefreshPage();
+    }      
+    public void NextPage()
+    {
+        highestShownRank = lowestShownRank + 1;
+        RefreshPage();
+    }
+    public void PreviewsPage()
+    {
+        highestShownRank = highestShownRank - 1 - shownRanks;
+        RefreshPage();
+    }
+    public async void RefreshPage()
+    {
+        LootLockerLeaderboardMember[] lootLockerLeaderboardMembers = await GetScores();
+        ShowScores(lootLockerLeaderboardMembers);
+    }
+    private async Task<LootLockerLeaderboardMember[]> GetScores()
+    {
+        LootLockerLeaderboardMember[] lootLockerLeaderboardMembers = await leaderboardController.GetScores(highestShownRank, shownRanks);
+        lowestShownRank = highestShownRank + lootLockerLeaderboardMembers.Length - 1;
+        return lootLockerLeaderboardMembers;
+    }
+    private void ShowScores(LootLockerLeaderboardMember[] lootLockerLeaderboardMembers)
+    {
+        for(int i=0;i< leaderboardMemberGOArray.Length; i++)
+        {
+            if(i< lootLockerLeaderboardMembers.Length)
+            {
+                ShowMemberScore(lootLockerLeaderboardMembers[i], leaderboardMemberGOArray[i]);
+                leaderboardMemberGOArray[i].SetActive(true);
+            }
+            else
+            {
+                leaderboardMemberGOArray[i].SetActive(false);
+            }
+        }
+    }
+    private void ShowMemberScore(LootLockerLeaderboardMember lootLockerLeaderboardMember,GameObject leaderboardMemberGO)
+    {
+        TMP_Text rank = leaderboardMemberGO.GetComponentsInChildren<TMP_Text>(true)[0];
+        TMP_Text user = leaderboardMemberGO.GetComponentsInChildren<TMP_Text>(true)[1];
+        TMP_Text score = leaderboardMemberGO.GetComponentsInChildren<TMP_Text>(true)[2];
+
+        rank.text = lootLockerLeaderboardMember.rank.ToString();
+        user.text = lootLockerLeaderboardMember.member_id;
+        score.text = lootLockerLeaderboardMember.score.ToString();
+    }
+}
